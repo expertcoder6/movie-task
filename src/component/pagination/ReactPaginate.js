a9177f1c355e3be52c6325e2c4c7fabcd3134c1e
hitesh <expertcoder6@gmail.com>
2024-08-09 18:20:35 +0530

    fixed issues

--- src/component/pagination/ReactPaginate.js
+++ src/component/pagination/ReactPaginate.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
+import Common from '@/pages/api/common';
 
 const ReactPaginate = ({ currentPage, totalPages, handlePaginate }) => {
     if (!totalPages) return null
+    const length = Math.ceil(totalPages / Common.pageSize);
     return (
         <Pagination className="custom-pagination">
             <Pagination.Prev onClick={() => handlePaginate(currentPage - 1)} className="pagination-text" disabled={totalPages === 1 || currentPage === 1}>Prev</Pagination.Prev>
-            {Array.from({ length: totalPages })?.map((_, index) => {
+            {Array.from({ length: length })?.map((_, index) => {
                 return <Pagination.Item onClick={() => handlePaginate(index + 1)} active={index + 1 === currentPage} key={index}>{index + 1}</Pagination.Item>
             })}
             <Pagination.Next onClick={() => handlePaginate(currentPage + 1)} className="pagination-text" disabled={totalPages === 1 || currentPage === totalPages}>Next</Pagination.Next>
